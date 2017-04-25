d7f6c0aed01251fd880fe31ca0c4a08e1343e2c2
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-04-25 12:39:21 -0600

    generalize plotting tools

--- issue_stats/get_stats.py
+++ issue_stats/get_stats.py
@@ -1,7 +1,26 @@
 import json
 import time
+from six.moves import input
 
 import requests
+
+session = requests.Session()
+
+username = input('Username: ')
+
+try:
+    import getpass
+    import keyring
+    pw = keyring.get_password('api.github.com', username)
+    if pw is None:
+        pw = getpass.getpass(prompt='Password for api.github.com: ')
+        keyring.set_password('api.github.com', username, pw)
+except ImportError:
+    password = input('Password (plaintext - if you want to hide it, install getpass & keyring): ')
+
+authorization = (username, pw)
+auth = session.get('https://api.github.com', auth=authorization)
+auth.raise_for_status()
 
 BASE_URL = "https://api.github.com/repos/astropy/astropy/issues"
 
@@ -16,7 +35,13 @@ params['page'] = 1
 
 while True:
 
-    results = requests.get(BASE_URL, params=params).json()
+    response = session.get(BASE_URL, params=params, auth=authorization)
+    if response.status_code == 403:
+        print('.', end='')
+        time.sleep(1)
+        continue
+    response.raise_for_status()
+    results = response.json()
 
     if len(results) == 0:
         break
@@ -26,13 +51,22 @@ while True:
         closed.append(issue['closed_at'])
 
     params['page'] += 1
+    print("Retrieved page {0} of closed issues".format(params['page']))
+    # slow it down so we don't exceed the rate limit imposed by the API
+    time.sleep(1)
 
 params['state'] = 'open'
 params['page'] = 1
 
 while True:
 
-    results = requests.get(BASE_URL, params=params).json()
+    response = session.get(BASE_URL, params=params, auth=authorization)
+    if response.status_code == 403:
+        print('.', end='')
+        time.sleep(1)
+        continue
+    response.raise_for_status()
+    results = response.json()
 
     if len(results) == 0:
         break
@@ -41,6 +75,10 @@ while True:
         created.append(issue['created_at'])
 
     params['page'] += 1
+
+    print("Retrieved page {0} of open issues".format(params['page']))
+    # slow it down so we don't exceed the rate limit imposed by the API
+    time.sleep(1)
 
 with open('created.txt', 'w') as f:
     for c in created:
